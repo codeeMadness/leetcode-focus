bc25f544da6f0f454b5c9390148b772bb14cd99d
codeeMadness <codee.madness@gmail.com>
2023-10-11 15:51:18 +0700

    update

--- practices/src/datastructures/arrays/_27.java
+++ practices/src/datastructures/arrays/_27.java
@@ -2,23 +2,41 @@ package datastructures.arrays;
 
 //https://leetcode.com/problems/remove-element/
 
+import java.util.Arrays;
+
 public class _27 {
 
-    public static int attempt() {
-        int[] nums = {3,2,2,3};
-        int val = 3;
-//       k = 2
-
-        //nums = [0,1,2,2,3,0,4,2]
-        //val = 2
-        //k = 5
-
-
+    public static int removeElement(int[] nums, int val) {
         int k = 0;
+
+        for(int num : nums) {
+            if(num != val) k++;
+        }
+
+        int indexVal = 0;
+        int i = 0;
+
+        while(i < nums.length && indexVal < nums.length) {
+            if(nums[indexVal] != val)
+                indexVal++;
+
+            if(nums[i] != val && i > indexVal) {
+                nums[indexVal] = nums[i];
+                nums[i] = val;
+            }
+            i++;
+        }
+
         return k;
     }
 
     public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val =  3;
 
+        int k = removeElement(nums, val);
+
+        nums = Arrays.copyOfRange(nums, 0, k);
+        System.out.println(Arrays.toString(nums));
     }
 }
