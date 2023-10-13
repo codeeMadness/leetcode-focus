c26a5cdcf536111977fec8e0317e3aa634182d24
codeeMadness <codee.madness@gmail.com>
2023-10-13 15:29:12 +0700

    update

--- practices/src/datastructures/arrays/_1051.java
+++ practices/src/datastructures/arrays/_1051.java
@@ -2,13 +2,24 @@ package datastructures.arrays;
 
 //https://leetcode.com/problems/height-checker/
 
+import java.util.Arrays;
+
 public class _1051 {
 
     public static int heightChecker(int[] heights) {
-        return 0;
+        int[] expected = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(expected);
+
+        int count = 0;
+        for(int i = 0; i < heights.length; i++) {
+            if(heights[i] != expected[i]) count++;
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {
-
+        int[] heights = {1,1,4,2,1,3};
+        System.out.println(heightChecker(heights));
     }
 }
--- practices/src/datastructures/arrays/_448.java
+++ practices/src/datastructures/arrays/_448.java
@@ -2,14 +2,50 @@ package datastructures.arrays;
 
 //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
-import java.util.List;
+import java.util.*;
 
 public class _448 {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-        return null;
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        Map items = new HashMap();
+        for(int num : nums) {
+            items.put(num, true);
+        }
+
+        for(int i = 1; i <= nums.length; i++) {
+            if(!items.containsKey(i)) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+
+    public static List<Integer> findDisappearedNumbers1(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        int[] arr = new int[nums.length];
+        arr = Arrays.stream(arr).map(x -> 0).toArray();
+
+        for(int num : nums) {
+            int index = num % nums.length;
+            arr[index] = 1;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(arr[i] == 0 && i == 0) result.add(arr.length);
+            if(arr[i] == 0 && i != 0) result.add(i);
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {4,3,2,7,8,2,3,1};
+//        int[] nums = {1,1};
+//        List<Integer> result = findDisappearedNumbers(nums);
+        List<Integer> result = findDisappearedNumbers1(nums);
+        result.forEach(System.out::println);
     }
 }
