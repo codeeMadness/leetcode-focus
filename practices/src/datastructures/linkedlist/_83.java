1428373ff56e31e27f78bb6026a86f8701db081e
codeeMadness <codee.madness@gmail.com>
2023-10-31 22:07:02 +0700

    update

---
+++ practices/src/datastructures/linkedlist/_83.java
@@ -0,0 +1,6 @@
+package datastructures.linkedlist;
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+public class _83 {
+}
---
+++ practices/src/datastructures/linkedlist/_92.java
@@ -0,0 +1,6 @@
+package datastructures.linkedlist;
+
+//https://leetcode.com/problems/reverse-linked-list-ii/
+
+public class _92 {
+}
