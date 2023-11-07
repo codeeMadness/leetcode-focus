c80be35e9d6b06c3a43462c34b9237726993b59e
codeeMadness <codee.madness@gmail.com>
2023-11-07 09:14:51 +0700

    update

--- practices/src/datastructures/linkedlist/_1721.java
+++ practices/src/datastructures/linkedlist/_1721.java
@@ -42,12 +42,34 @@ public class _1721 {
         return head;
     }
 
+    public static ListNode swapNodes2(ListNode head, int k) {
+        ListNode n1 = null, n2 = null;
+
+        for (var current = head; current != null; current = current.next) {
+
+            if (n2 != null)
+                n2 = n2.next;
+
+            if (--k == 0) {
+                n1 = current;
+                n2 = head;
+            }
+        }
+
+        // The problem description specifically asks to swap values, not nodes themselves.
+        var tmp = n1.val;
+        n1.val = n2.val;
+        n2.val = tmp;
+        return head;
+    }
+
+
     public static void main(String[] args) {
-//        ListNode head = ListNode.convert(new int[] {1,2,3,4,5});
-        ListNode head = ListNode.convert(new int[] {7,9,6,6,7,8,3,0,9,5});
+        ListNode head = ListNode.convert(new int[] {1,2,3,4,5});
+//        ListNode head = ListNode.convert(new int[] {7,9,6,6,7,8,3,0,9,5});
 
 //        ListNode head = new ListNode(1, new ListNode(2));
-        ListNode result = swapNodes(head, 1);
+        ListNode result = swapNodes2(head, 2);
         ListNode.print(result);
 
     }
