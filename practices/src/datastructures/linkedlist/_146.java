80c6ea46b238833dd39862937b7446e27f8a0123
codeeMadness <codee.madness@gmail.com>
2023-11-20 10:51:27 +0700

    update

--- practices/src/datastructures/linkedlist/_146.java
+++ practices/src/datastructures/linkedlist/_146.java
@@ -7,48 +7,83 @@ import java.util.Map;
 
 public class _146 {
 
-    static class LRUCache {
-        private int capacity = 0;
-        private int currentSize = 0;
-        private ListNode<Integer> head = new ListNode<>();
-        private ListNode<Integer> tail = head;
+    static class Node {
+        private int key;
+        private int val;
+        private Node next;
+        private Node prev;
+        public Node(int key, int val, Node next, Node prev) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
 
-        private final Map<Integer, Integer> data = new HashMap<>();
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class LRUCache {
+        private int capacity;
+        private Node dummyHead;
+        private Node dummyTail;
+        private Map<Integer, Node> map;
 
         public LRUCache(int capacity) {
             this.capacity = capacity;
+            this.dummyHead = new Node(-1, -1);
+            this.dummyTail = new Node(-1, -1);
+            this.dummyHead.next = dummyTail;
+            this.dummyTail.prev = this.dummyHead;
+            this.map = new HashMap<>();
         }
 
         public int get(int key) {
-            ListNode<Integer> pointer = head;
-            while(pointer != null) {
-                if(pointer.val == key) {
-                    tail.next = pointer;
-                    tail = tail.next;
-                }
-                pointer = pointer.next;
+            if(map.containsKey(key)) {
+                Node node = map.get(key);
+                remove(node);
+                add(node);
+                return node.val;
             }
-            return data.getOrDefault(key, -1);
+            return -1;
         }
 
         public void put(int key, int value) {
-            if(currentSize == capacity) {
-                data.remove(head.val);
-                head = head.next;
-                currentSize--;
+            //remove current node if exists
+            if(map.containsKey(key))
+                remove(map.get(key));
+
+            //add node at front
+            Node node = new Node(key, value);
+            add(node);
+            map.put(key, node);
+
+            //remove last node if capacity is full
+            if(map.size() > capacity) {
+                Node lastNode = this.dummyTail.prev;
+                int theKey = lastNode.key;
+                map.remove(theKey);
+                remove(lastNode);
             }
+        }
 
-            data.put(key, value);
+        private void add(Node node) {
+            Node firstNode = this.dummyHead.next;
+            this.dummyHead.next = node;
+            firstNode.prev = node;
+            node.next = firstNode;
+            node.prev = this.dummyHead;
+        }
 
-            ListNode<Integer> newNode = new ListNode<>(key);
-            if(currentSize == 0)
-                head = newNode;
-            else {
-                tail.next = newNode;
-                tail = tail.next;
-            }
-
-            currentSize++;
+        private void remove(Node node) {
+            Node prevNode = node.prev;
+            Node nextNode = node.next;
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
         }
     }
 
@@ -56,13 +91,13 @@ public class _146 {
         LRUCache lRUCache = new LRUCache(2);
         lRUCache.put(1, 1); // cache is {1=1}
         lRUCache.put(2, 2); // cache is {1=1, 2=2}
-        lRUCache.get(1);    // return 1
+        System.out.println(lRUCache.get(1));    // return 1
         lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-        lRUCache.get(2);    // returns -1 (not found)
+        System.out.println(lRUCache.get(2));    // returns -1 (not found)
         lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-        lRUCache.get(1);    // return -1 (not found)
-        lRUCache.get(3);    // return 3
-        lRUCache.get(4);    // return 4
+        System.out.println(lRUCache.get(1));    // return -1 (not found)
+        System.out.println(lRUCache.get(3));    // return 3
+        System.out.println(lRUCache.get(4));    // return 4
     }
 
 }
