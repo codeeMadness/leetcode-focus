d4a8c932111820a22d06e8c24057b5a16118d1a1
codeeMadness <codee.madness@gmail.com>
2023-11-24 17:30:43 +0700

    update

--- practices/src/datastructures/linkedlist/_460.java
+++ practices/src/datastructures/linkedlist/_460.java
@@ -1,19 +1,74 @@
 package datastructures.linkedlist;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //https://leetcode.com/problems/lfu-cache/
 public class _460 {
+
+    static class Node {
+        private int key;
+        private int val;
+
+        private int cnt;
+        private Node next;
+        private Node prev;
+
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+            this.next = null;
+            this.prev = null;
+            this.cnt = 0;
+        }
+
+        public Node(int key, int val, int cnt) {
+            this.key = key;
+            this.val = val;
+            this.next = null;
+            this.prev = null;
+            this.cnt = cnt;
+        }
+    }
+
     static class LFUCache {
+        private int capacity;
+        private Node dummyHead;
+        private Node dummyTail;
+        private Map<Integer, Node> map;
 
         public LFUCache(int capacity) {
-
+            this.capacity = capacity;
+            this.dummyHead = new Node(-1, -1);
+            this.dummyTail = new Node(-1, -1);
+            this.dummyHead.next = dummyTail;
+            this.dummyTail.prev = this.dummyHead;
+            this.map = new HashMap<>();
         }
 
         public int get(int key) {
+            map.get(key);
             return -1;
         }
 
         public void put(int key, int value) {
+            Node node = new Node(key, value, cnt(key));
+            map.put(key, node);
+        }
 
+        private int cnt(int key) {
+            return 1;
+        }
+
+        private void add(Node node) {
+
+        }
+
+        private void remove(Node node) {
+            Node prevNode = node.prev;
+            Node nextNode = node.next;
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
         }
     }
 
