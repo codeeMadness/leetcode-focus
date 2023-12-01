d03f521c2eb8e2e4db4c85b32a3a487616216a19
codeeMadness <codee.madness@gmail.com>
2023-12-01 16:37:47 +0700

    update

--- practices/src/Algorithm/LineSweep/_1893.java
+++ practices/src/Algorithm/LineSweep/_1893.java
@@ -4,13 +4,13 @@ package Algorithm.LineSweep;
 
 public class _1893 {
     public static boolean isCovered(int[][] ranges, int left, int right) {
-        int[] line = new int[52];
-        for (var r : ranges) {
-            line[r[0]] += 1;
-            line[r[1] + 1] -= 1;
+        int[] sweepLine = new int[52];
+        for (var range : ranges) {
+            sweepLine[range[0]] += 1;
+            sweepLine[range[1] + 1] -= 1;
         }
         for (int i = 1, overlaps = 0; i <= right; ++i) {
-            overlaps += line[i];
+            overlaps += sweepLine[i];
             if (i >= left && overlaps == 0)
                 return false;
         }
