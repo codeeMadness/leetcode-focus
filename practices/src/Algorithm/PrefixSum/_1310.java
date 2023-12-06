502ac0b0da5585f8c22ce13d3e87fd021e60bddf
codeeMadness <codee.madness@gmail.com>
2023-12-06 21:24:27 +0700

    update

---
+++ practices/src/Algorithm/PrefixSum/_1310.java
@@ -0,0 +1,9 @@
+package Algorithm.PrefixSum;
+
+//https://leetcode.com/problems/xor-queries-of-a-subarray/
+
+public class _1310 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ practices/src/Algorithm/PrefixSum/_2391.java
@@ -0,0 +1,9 @@
+package Algorithm.PrefixSum;
+
+//https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/
+
+public class _2391 {
+    public static void main(String[] args) {
+
+    }
+}
