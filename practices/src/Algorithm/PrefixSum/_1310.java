9b1fd3e3d65f100a55006a7c340e02c874c9bb4f
codeeMadness <codee.madness@gmail.com>
2023-12-12 16:58:43 +0700

    update

--- practices/src/Algorithm/PrefixSum/_1310.java
+++ practices/src/Algorithm/PrefixSum/_1310.java
@@ -2,8 +2,25 @@ package Algorithm.PrefixSum;
 
 //https://leetcode.com/problems/xor-queries-of-a-subarray/
 
-public class _1310 {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class _1310 {
+    public static int[] xorQueries(int[] arr, int[][] queries) {
+        //prefix XOR
+        for (int i = 1; i < arr.length; ++i)
+            arr[i] ^= arr[i - 1];
+
+        int[] res = new int[queries.length], query;
+        for (int i = 0; i < queries.length; ++i) {
+            query = queries[i];
+            res[i] = query[0] > 0 ? arr[query[1]] ^ arr[query[0] - 1] : arr[query[1]];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+//        int[] arr = {1,3,4,8}, queries[] = {{0,1},{1,2},{0,3},{3,3}}; //[2,7,14,8]
+        int[] arr = {4,8,2,10}, queries[] = {{2,3},{1,3},{0,0},{0,3}}; //[8,0,4,4]
+        System.out.println(Arrays.toString(xorQueries(arr, queries)));
     }
 }
--- practices/src/Algorithm/PrefixSum/_2391.java
+++ practices/src/Algorithm/PrefixSum/_2391.java
@@ -3,7 +3,29 @@ package Algorithm.PrefixSum;
 //https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/
 
 public class _2391 {
-    public static void main(String[] args) {
+    public static int garbageCollection(String[] garbage, int[] travel) {
+        int last[] = new int[128], res = 0;
+        for (int i = 0; i < garbage.length; ++i) {
+            res += garbage[i].length();
+            for (int j = 0; j < garbage[i].length(); ++j)
+                last[garbage[i].charAt(j)] = i;
+        }
 
+        //prefix sum for travel time
+        for (int j = 1; j < travel.length; ++j)
+            travel[j] += travel[j - 1];
+
+        for (int c : "PGM".toCharArray())
+            if (last[c] > 0)
+                res += travel[last[c] - 1];
+        return res;
+    }
+    public static void main(String[] args) {
+//        String[] garbage = {"G","P","GP","GG"}; //21
+//        int[] travel = {2,4,3};
+
+        String[] garbage = {"MMM","PGM","GP"}; //37
+        int[] travel = {3,10};
+        System.out.println(garbageCollection(garbage, travel));
     }
 }
