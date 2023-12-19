3f276221572df1482df234dfcaefc5031d373b0e
codeeMadness <codee.madness@gmail.com>
2023-12-19 22:14:06 +0700

    update

--- practices/src/DataStructures/Stack/_503.java
+++ practices/src/DataStructures/Stack/_503.java
@@ -7,19 +7,29 @@ import java.util.Stack;
 public class _503 {
 
     public static  int[] nextGreaterElements(int[] nums) {
-        int n = nums.length, res[] = new int[n];
+
+        int n = nums.length;
+        int res[] = new int[n];
+
         Arrays.fill(res, -1);
+
         Stack<Integer> stack = new Stack<>();
+
         for (int i = 0; i < n * 2; i++) {
+
             while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n])
                 res[stack.pop()] = nums[i % n];
+
             stack.push(i % n);
+
         }
+
         return res;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,1};
+//        int[] nums = {1,2,1}; //[2,-1,2]
+        int[] nums = {1,2,3,4,3}; //[2,3,4,-1,4]
         System.out.println(Arrays.toString(nextGreaterElements(nums)));
     }
 }
