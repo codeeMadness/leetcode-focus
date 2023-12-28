d946330c41b6aa05f31856033fb5ea739a07e5ac
codeeMadness <codee.madness@gmail.com>
2023-12-28 16:16:00 +0700

    update

--- practices/src/DataStructures/Stack/_1249.java
+++ practices/src/DataStructures/Stack/_1249.java
@@ -2,12 +2,33 @@ package DataStructures.Stack;
 
 //https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
 
+import java.util.Stack;
+
 public class _1249 {
     public static String minRemoveToMakeValid(String s) {
-        return "";
+        String[] arr = s.split("");
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i <arr.length; i++) {
+            if(arr[i].equals(")")) {
+                if(!stack.isEmpty()) stack.pop();
+                else arr[i] = "";
+            }
+            if(arr[i].equals("("))
+                stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            arr[stack.pop()] = "";
+        }
+
+        return String.join("",arr);
     }
 
     public static void main(String[] args) {
-
+//        String s = "lee(t(c)o)de)"; //"lee(t(c)o)de"
+//        String s = "a)b(c)d"; //"ab(c)d"
+        String s = "))(("; //""
+        System.out.println(minRemoveToMakeValid(s));
     }
 }
