d5bc7f7def24f957c956af86eb9626c1c3146035
codeeMadness <codee.madness@gmail.com>
2024-01-08 22:28:09 +0700

    update

--- practices/src/Algorithm/Recursion/_203.java
+++ practices/src/Algorithm/Recursion/_203.java
@@ -7,7 +7,8 @@ public class _203 {
     public static ListNode<Integer> removeElements(ListNode<Integer> head, int val) {
         ListNode<Integer> dummy = new ListNode<>(-1);
         dummy.next = head;
-        return remove(dummy, head, val);
+        remove(dummy, head, val);
+        return dummy.next;
     }
 
     public static ListNode<Integer> remove(ListNode<Integer> prev, ListNode<Integer> curr, int val) {
@@ -22,9 +23,8 @@ public class _203 {
 
     public static void main(String[] args) {
 //        ListNode<Integer> head = ListNode.convert(1,2,6,3,4,5,6); int val = 6;
-//        ListNode<Integer> head = null; int val = 1;
-        ListNode<Integer> head = ListNode.convert(7,7,7,7); int val = 7;
-        removeElements(head, val);
-        ListNode.print(head);
+        ListNode<Integer> head = null; int val = 1;
+//        ListNode<Integer> head = ListNode.convert(7,7,7,7); int val = 7;
+        ListNode.print(removeElements(head, val));
     }
 }
---
+++ practices/src/Algorithm/Recursion/_24.java
@@ -0,0 +1,41 @@
+package Algorithm.Recursion;
+
+//https://leetcode.com/problems/swap-nodes-in-pairs/
+
+import DataStructures.LinkedList.ListNode;
+
+public class _24 {
+    public static ListNode<Integer> swapPairs(ListNode<Integer> head) {
+        ListNode<Integer> dummy = new ListNode<>(-999);
+        dummy.next = head;
+        swap(dummy, head);
+        return dummy.next;
+    }
+
+    private static ListNode<Integer> swap(ListNode<Integer> prev, ListNode<Integer> first) {
+        if(first == null || first.next == null) return null;
+        ListNode<Integer> temp = first.next;
+        first.next = first.next.next;
+        temp.next = first;
+        first = temp;
+        prev.next = first;
+        return swap(first.next, first.next.next);
+    }
+
+    public ListNode swapPairs2(ListNode head) {
+        if ((head == null)||(head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+
+    public static void main(String[] args) {
+//        ListNode<Integer> head = ListNode.convert(1,2,3,4,5,6,7);
+//        ListNode<Integer> head = ListNode.convert(1);
+        ListNode<Integer> head = ListNode.convert();
+        ListNode<Integer> newHead = swapPairs(head);
+        ListNode.print(newHead);
+    }
+}
