461b1ac5da632dbbcb4fbf1d43276b3b9d162c3f
codeeMadness <codee.madness@gmail.com>
2024-01-15 19:57:48 +0700

    update

--- practices/src/Algorithm/BackTracking/_39.java
+++ practices/src/Algorithm/BackTracking/_39.java
@@ -6,32 +6,23 @@ import java.util.*;
 
 public class _39 {
 
-    static int index = 0;
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         backtrack(result, new ArrayList<>(), candidates, target, 0);
         return result;
     }
 
-    private static void backtrack(List<List<Integer>> result, List<Integer> option, int[] candidates, int target, int sum) {
-        if(sum == target){
-            result.add(new ArrayList<>(option));
-            index++;
-            return;
-        }
+    private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
 
-        for(int i = index; i < candidates.length; i++) {
-
-            if(sum + candidates[i] <= target) {
-                sum += candidates[i];
-                option.add(candidates[i]);
-                backtrack(result, option, candidates, target, sum);
-
-                option.remove(option.size()-1);
-                sum -= candidates[i];
+        if(remain == 0)
+            list.add(new ArrayList<>(tempList));
+        else{
+            for(int i = start; i < nums.length; i++){
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
             }
-
-
         }
     }
 
