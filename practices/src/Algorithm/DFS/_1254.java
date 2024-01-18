2e0a89b2716cf3e35579ce949abea8a14c998b5e
codeeMadness <codee.madness@gmail.com>
2024-01-18 13:42:58 +0700

    update

--- practices/src/Algorithm/DFS/_1254.java
+++ practices/src/Algorithm/DFS/_1254.java
@@ -3,5 +3,51 @@ package Algorithm.DFS;
 //https://leetcode.com/problems/number-of-closed-islands/description/
 
 public class _1254 {
+    public static int closedIsland(int[][] grid) {
+        int rows = grid.length;
+        int columns = grid[0].length;
+
+        int ans = 0;
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < columns; c++) {
+                if(grid[r][c] == 0)
+                    ans += dfs(grid,r,c) ? 1 : 0;
+            }
+        }
+
+        return ans;
+
+    }
+
+    private static boolean dfs(int[][] grid, int r, int c) {
+        int rows = grid.length;
+        int columns = grid[0].length;
+
+        if(r < 0 || r >= rows || c < 0 || c >= columns) return false;
+
+        if(grid[r][c] == 1) return true;
+
+        grid[r][c] = 1;
+
+        boolean goLeft = dfs(grid, r, c-1);
+        boolean goRight = dfs(grid, r, c+1);
+        boolean goTop = dfs(grid, r-1, c);
+        boolean goBottom = dfs(grid, r+1, c);
+
+        return goLeft && goRight && goTop && goBottom;
+    }
+
+    public static void main(String[] args) {
+//        int[][] grid = {{1,1,1,1,1,1,1,0},{1,0,0,0,0,1,1,0},{1,0,1,0,1,1,1,0},{1,0,0,0,0,1,0,1},{1,1,1,1,1,1,1,0}}; //2
+//        int[][] grid = {{0,0,1,0,0},{0,1,0,1,0},{0,1,1,1,0}}; //1
+        int[][] grid = {{1,1,1,1,1,1,1},
+               {1,0,0,0,0,0,1},
+               {1,0,1,1,1,0,1},
+               {1,0,1,0,1,0,1},
+               {1,0,1,1,1,0,1},
+               {1,0,0,0,0,0,1},
+               {1,1,1,1,1,1,1}}; //2
+        System.out.println(closedIsland(grid));
+    }
 
 }
--- practices/src/Algorithm/DFS/_419.java
+++ practices/src/Algorithm/DFS/_419.java
@@ -3,4 +3,42 @@ package Algorithm.DFS;
 //https://leetcode.com/problems/battleships-in-a-board/description/
 
 public class _419 {
+    public static int countBattleships(char[][] board) {
+        int rows = board.length;
+        int columns = board[0].length;
+        int count = 0;
+
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < columns; c++) {
+                if(board[r][c] == 'X') {
+                    count++;
+                    dfs(board, r, c);
+                }
+            }
+        }
+
+        return count;
+
+    }
+
+    private static void dfs(char[][] board, int r, int c) {
+        int rows = board.length;
+        int columns = board[0].length;
+
+        if(r < 0 || r >= rows || c < 0 || c >= columns) return;
+        if(board[r][c] == '.') return;
+
+        board[r][c] = '.';
+
+        dfs(board, r-1, c); //up
+        dfs(board, r+1, c); //down
+        dfs(board, r, c-1); //left
+        dfs(board, r, c+1); //right
+
+    }
+    public static void main(String[] args) {
+//        char[][] board = {{'X','.','.','X'},{'.','.','.','X'},{'.','.','.','X'}}; //2
+        char[][] board = {{'.'}};//0
+        System.out.println(countBattleships(board));
+    }
 }
