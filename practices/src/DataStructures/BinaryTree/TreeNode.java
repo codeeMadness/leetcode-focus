b1624dfae6862da769df8c380a62beef4b1c65a7
codeeMadness <codee.madness@gmail.com>
2024-01-22 22:17:04 +0700

    update

--- practices/src/DataStructures/BinaryTree/TreeNode.java
+++ practices/src/DataStructures/BinaryTree/TreeNode.java
@@ -1,14 +1,49 @@
 package DataStructures.BinaryTree;
 
-public class TreeNode {
-      int val;
+public class TreeNode<T> {
+      T val;
       TreeNode left;
       TreeNode right;
       TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
+      TreeNode(T val) { this.val = val; }
+      TreeNode(T val, TreeNode left, TreeNode right) {
           this.val = val;
           this.left = left;
           this.right = right;
+      }
+
+      public static <T> TreeNode convert(T[] array) {
+          if(array.length == 0 || null == array[0]) return null;
+          int i = 0;
+          TreeNode root = new TreeNode(array[i]);
+
+          dfs(array, root, i+1, true);
+          dfs(array, root, i+2, false);
+
+          return root;
+      }
+
+      private static <T> void dfs(T[] array, TreeNode root, int i, boolean isLeft) {
+            if(i >= array.length || array[i] == null) return;
+
+            TreeNode node = new TreeNode(array[i]);
+            array[i] = null;
+
+            if(isLeft) {
+                root.left = node;
+                root = root.left;
+            } else {
+                root.right = node;
+                root = root.right;
+            }
+
+            dfs(array, root, i+1, true);
+            dfs(array, root, i+2, false);
+      }
+
+      public static void main(String[] args) {
+            Integer[] array = {1,null,2,3,4,5};
+            TreeNode root = TreeNode.convert(array);
+            System.out.println(root);
       }
 }
--- practices/src/DataStructures/BinaryTree/_144.java
+++ practices/src/DataStructures/BinaryTree/_144.java
@@ -3,5 +3,26 @@ package DataStructures.BinaryTree;
 //https://leetcode.com/problems/binary-tree-preorder-traversal/description/
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _144 {
+    public static List<Integer> preorderTraversal(TreeNode<Integer> root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(result, root);
+        return result;
+    }
+
+    private static void preorder(List<Integer> result, TreeNode<Integer> root) {
+        if(null == root)
+            return;
+        result.add(root.val);
+        preorder(result, root.left);
+        preorder(result, root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(1, null, new TreeNode(2, new TreeNode(3), null));
+        preorderTraversal(root).forEach(System.out::println);
+    }
 }
