710c8b6e8f3ad4510871a4b53b55ee2801576ae8
codeeMadness <codee.madness@gmail.com>
2024-01-23 15:12:39 +0700

    update

--- practices/src/DataStructures/BinaryTree/_145.java
+++ practices/src/DataStructures/BinaryTree/_145.java
@@ -2,5 +2,24 @@ package DataStructures.BinaryTree;
 
 //https://leetcode.com/problems/binary-tree-postorder-traversal/description/
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _145 {
+    public static List<Integer> postorderTraversal(TreeNode<Integer> root) {
+        List<Integer> result = new ArrayList<>();
+        postOrder(result, root);
+        return result;
+    }
+
+    private static void postOrder(List<Integer> result, TreeNode<Integer> root) {
+        if(root == null) return;
+        postOrder(result, root.left);
+        postOrder(result, root.right);
+        result.add(root.val);
+    }
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode<>(3, new TreeNode(6), null));
+        postorderTraversal(root).forEach(System.out::println); //[4,5,2,6,3,1]
+    }
 }
--- practices/src/DataStructures/BinaryTree/_94.java
+++ practices/src/DataStructures/BinaryTree/_94.java
@@ -2,5 +2,24 @@ package DataStructures.BinaryTree;
 
 //https://leetcode.com/problems/binary-tree-inorder-traversal/description/
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _94 {
+    public static List<Integer> inorderTraversal(TreeNode<Integer> root) {
+        List<Integer> result = new ArrayList<>();
+        inOrder(result, root);
+        return result;
+    }
+
+    private static void inOrder(List<Integer> result, TreeNode<Integer> root) {
+        if(null == root) return;
+        inOrder(result, root.left);
+        result.add(root.val);
+        inOrder(result, root.right);
+    }
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode<>(3, new TreeNode(6), null));
+        inorderTraversal(root).forEach(System.out::println); //425163
+    }
 }
