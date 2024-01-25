922b194996679330146fb5013d42375eb29b9b44
codeeMadness <codee.madness@gmail.com>
2024-01-25 18:35:16 +0700

    update

--- practices/src/DataStructures/BinaryTree/_617.java
+++ practices/src/DataStructures/BinaryTree/_617.java
@@ -3,4 +3,25 @@ package DataStructures.BinaryTree;
 //https://leetcode.com/problems/merge-two-binary-trees/
 
 public class _617 {
+
+    public static TreeNode<Integer> mergeTrees(TreeNode<Integer> root1, TreeNode<Integer> root2) {
+        if (root1 == null && root2 == null) return null;
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+
+        TreeNode new_node = new TreeNode(root1.val + root2.val);
+
+        new_node.left = mergeTrees(root1.left, root2.left);
+        new_node.right = mergeTrees(root1.right, root2.right);
+
+        return new_node;
+    }
+
+    public static void main(String[] args) {
+        TreeNode<Integer> root1 = new TreeNode<>(1, new TreeNode(3, new TreeNode(5) , null), new TreeNode(2));
+        TreeNode<Integer> root2 = new TreeNode<>(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
+        TreeNode<Integer> newRoot = mergeTrees(root1, root2);
+        System.out.println(newRoot);
+
+    }
 }
--- practices/src/DataStructures/BinaryTree/_897.java
+++ practices/src/DataStructures/BinaryTree/_897.java
@@ -3,4 +3,31 @@ package DataStructures.BinaryTree;
 //https://leetcode.com/problems/increasing-order-search-tree/description/
 
 public class _897 {
+
+    public static TreeNode increasingBST(TreeNode root) {
+        increasingBST(root, null);
+        return root;
+    }
+
+    public static TreeNode increasingBST(TreeNode root, TreeNode tail) {
+        if (root == null) return tail;
+        TreeNode res = increasingBST(root.left, root);
+        root.left = null;
+        root.right = increasingBST(root.right, tail);
+        return res;
+    }
+
+    public static void inOrder(TreeNode dummy, TreeNode<Integer> root) {
+        if(root == null) return;
+        inOrder(dummy, root.left);
+        dummy = root;
+        inOrder(dummy.right, root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode<Integer> root =
+                new TreeNode<>(5, new TreeNode(3, new TreeNode(2, new TreeNode(1), null), new TreeNode(4)), new TreeNode(6, null, new TreeNode(8, new TreeNode(7), new TreeNode(9))));
+        System.out.println(increasingBST(root));
+
+    }
 }
