03156843f8e0da31be0b8bdf1d861bf2082d1cee
codeeMadness <codee.madness@gmail.com>
2024-03-07 17:05:25 +0700

    update

---
+++ practices/src/DataStructures/BinaryTree/_101.java
@@ -0,0 +1,12 @@
+package DataStructures.BinaryTree;
+
+//https://leetcode.com/problems/symmetric-tree/description/
+public class _101 {
+    public static boolean isSymmetric(TreeNode root) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ practices/src/DataStructures/BinaryTree/_104.java
@@ -0,0 +1,13 @@
+package DataStructures.BinaryTree;
+
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+public class _104 {
+
+    public static int maxDepth(TreeNode root) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
