0ecf43356e73489ca87c817ec5bf6c3ac3034b1c
codeeMadness <codee.madness@gmail.com>
2024-03-13 21:04:22 +0700

    update

--- practices/src/DataStructures/BinaryTree/_101.java
+++ practices/src/DataStructures/BinaryTree/_101.java
@@ -3,10 +3,17 @@ package DataStructures.BinaryTree;
 //https://leetcode.com/problems/symmetric-tree/description/
 public class _101 {
     public static boolean isSymmetric(TreeNode root) {
+        return compare(root.left, root.right);
+    }
 
+    private static boolean compare(TreeNode<Integer> left, TreeNode<Integer> right) {
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        return compare(left.right, right.left) && compare(left.left, right.right);
     }
 
     public static void main(String[] args) {
-
+        TreeNode<Integer> tree = new TreeNode<>(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(3), new TreeNode(4)));
+        System.out.println(isSymmetric(tree));
     }
 }
--- practices/src/DataStructures/BinaryTree/_104.java
+++ practices/src/DataStructures/BinaryTree/_104.java
@@ -4,10 +4,14 @@ package DataStructures.BinaryTree;
 public class _104 {
 
     public static int maxDepth(TreeNode root) {
-
+        if(root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
-    public static void main(String[] args) {
+    //TODO: taily recursion
 
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        System.out.println(maxDepth(root));
     }
 }
