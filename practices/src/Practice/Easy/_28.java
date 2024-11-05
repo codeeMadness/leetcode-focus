6ce333ec969c7850f6c619c2352cea1c061a1331
codee.madness <codee.madness@gmail.com>
2024-11-05 18:19:29 +0700

    update

---
+++ practices/src/Practice/Easy/_28.java
@@ -0,0 +1,13 @@
+package Practice.Easy;
+
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+public class _28 {
+    public static int strStr(String haystack, String needle) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
