9813552d840e263a5deafbc87532a48a726ffcd9
codee.madness <codee.madness@gmail.com>
2024-12-26 11:41:28 +0700

    update

--- practices/src/Practice/Easy/_28.java
+++ practices/src/Practice/Easy/_28.java
@@ -2,12 +2,19 @@ package Practice.Easy;
 
 //https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _28 {
     public static int strStr(String haystack, String needle) {
         return 0;
     }
 
     public static void main(String[] args) {
-
+        List<Integer> one = new ArrayList<>();
+        one.add(1); one.add(2); one.add(3);
+        List<Integer> two = new ArrayList<>(one);
+        one.clear();
+        System.out.println(one.equals(two));
     }
 }
